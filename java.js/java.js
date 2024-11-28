8c3a1a8fd1452e0fd2358fff7250485c1157cd5e
luiz662 <141882626+luiz662@users.noreply.github.com>
2024-11-27 21:15:21 -0300

    Add files via upload

--- java.js/java.js
+++ java.js/java.js
@@ -1,25 +1,28 @@
+const inicioPrograma =  Number(prompt(`
+        ======Bem-vindo======
+
+O que deseja comprar ? 
+=========================
+[1] - banana
+[2] - maça
+[3] - manga
+[4] - laranja
+[5] - limão
+    
+ 
+    `))
+    
 
 
-const inicioPrograma = Number(prompt("Bem-vindo! O que deseja comprar?\n[1] - Banana\n[2] - Maçã\n[3] - Manga\n[4] - Laranja\n[5] - Limão"));
-
-const frutas = ["Banana", "Maçã", "Manga", "Laranja", "Limão"];
-
-switch (inicioPrograma) {
+const frutas = [ " banana", "maçã", "laranja", "limão" ]; 
+switch(inicioPrograma){
     case 1:
-        alert("Você escolheu banana!");
-        break;
-    case 2:
-        alert("Você escolheu maçã!");
-        break;
-    case 3:
-        alert("Você escolheu manga!");
-        break;
-    case 4:
-        alert("Você escolheu laranja!");
-        break;
-    case 5:
-        alert("Você escolheu limão!");
-        break;
-    default:
-        alert("Opção inválida!");
+        alert("voce escolheu banana")
+}
+
+    
+
+for (let index = 0; index < frutas.length; index++) {
+    document.write(frutas[index])
+    
 }
